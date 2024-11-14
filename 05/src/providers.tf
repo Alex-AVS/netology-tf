35ecd14b6654fc9ac08bd5b2d8912893319f7893
Alex Sigachev <p5yspy@yandex.ru>
2024-11-14 15:35:02 +0300

    Fix lint errors

--- 05/src/providers.tf
+++ 05/src/providers.tf
@@ -2,8 +2,14 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = "~>0.130.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "~>2.2.0"
     }
   }
+
   required_version = "~>1.8.4"
 
   backend "s3" {
